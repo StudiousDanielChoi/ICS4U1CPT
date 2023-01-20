1d55f6c82af7651795a0eff34062f439a3fe60d1
StudiousDanielChoi <123113887+StudiousDanielChoi@users.noreply.github.com>
2023-01-20 13:44:03 -0500

    Update MainMethod.java

--- MainMethod.java
+++ MainMethod.java
@@ -3,8 +3,10 @@ public class MainMethod
     public static void main(String args[])
     {
         //done by Solomon
+        // creates new object of the KW class named loyolaKW using the constructor method
         KW loyolaKW = new KW(500, 20, 6, 21, 254, 1092);
-
+        
+        // prompting accessor methods, showing the value  and what it stands for
         System.out.println("This is the amount of kilowatts per day for Loyola.");
         System.out.println(loyolaKW.getKiloPerDay());
         System.out.println("This is the amount of kilowatts per month for Loyola.");
@@ -17,15 +19,23 @@ public class MainMethod
         System.out.println(loyolaKW.getCO2PerStudent());
         System.out.println("This is the amount of trees needed to compensate for the amount of carbon dioxide produced.");
         System.out.println(loyolaKW.getNumberTrees());
-
+        
+        // example of toString method working
         System.out.println(loyolaKW.toString());
-
+        
+        // example of modifier method working
         loyolaKW.changeLightbulb(100);
-
+        
+        // shows how the modifier method affects the program as a whole
         System.out.println(loyolaKW.toString());
         
         //done by Daniel
+        
+        //creates new object from the KW class
         KW gonzagaKW = new KW(1324, 40, 6, 21, 254, 1503);
+        
+        // prompting accessor methods, showing the value  and what it stands for
+        
         System.out.println("This is the amount of kilowatts per day for Loyola.");
         System.out.println(gonzagaKW.getKiloPerDay());
         System.out.println("This is the amount of kilowatts per month for Loyola.");
@@ -42,7 +52,10 @@ public class MainMethod
         System.out.println(gonzagaKW.toString());
         
         //done by Solomon
+        //creates new object from the KW class using the constructor method
         KW stfxKW = new KW(3202, 40, 6, 21, 254, 2321);
+        
+        // prompting accessor methods, showing the value  and what it stands for
         System.out.println("This is the amount of kilowatts per day for Loyola.");
         System.out.println(stfxaKW.getKiloPerDay());
         System.out.println("This is the amount of kilowatts per month for Loyola.");
@@ -56,6 +69,7 @@ public class MainMethod
         System.out.println("This is the amount of trees needed to compensate for the amount of carbon dioxide produced.");
         System.out.println(stfxKW.getNumberTrees());
 
+        // example of toString method working
         System.out.println(stfxKW.toString());
     }
 }

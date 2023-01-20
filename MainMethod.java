04740448ebc8066a31e5d8a0550e55b71f2528ec
StudiousDanielChoi <123113887+StudiousDanielChoi@users.noreply.github.com>
2023-01-20 00:29:09 -0500

    Update MainMethod.java

--- MainMethod.java
+++ MainMethod.java
@@ -22,6 +22,37 @@ public class MainMethod
         loyolaKW.changeLightbulb(100);
 
         System.out.println(loyolaKW.toString());
+        
+        KW gonzagaKW = new KW(1324, 40, 6, 21, 254, 1503);
+        System.out.println("This is the amount of kilowatts per day for Loyola.");
+        System.out.println(gonzagaKW.getKiloPerDay());
+        System.out.println("This is the amount of kilowatts per month for Loyola.");
+        System.out.println(gonzagaKW.getKiloPerMonth());
+        System.out.println("This is the amount of kilowatts per year for Loyola.");
+        System.out.println(gonzagaKW.getKiloPerYear());
+        System.out.println("This is the amount of carbon dioxide produced per year for Loyola.");
+        System.out.println(gonzagaKW.getCO2PerYear());
+        System.out.println("This is the amount of carbon dioxide a student produces at Loyola.");
+        System.out.println(gonzagaKW.getCO2PerStudent());
+        System.out.println("This is the amount of trees needed to compensate for the amount of carbon dioxide produced.");
+        System.out.println(gonzagaKW.getNumberTrees());
 
+        System.out.println(gonzagaKW.toString());
+        
+        KW stfxKW = new KW(3202, 40, 6, 21, 254, 2321);
+        System.out.println("This is the amount of kilowatts per day for Loyola.");
+        System.out.println(stfxaKW.getKiloPerDay());
+        System.out.println("This is the amount of kilowatts per month for Loyola.");
+        System.out.println(stfxKW.getKiloPerMonth());
+        System.out.println("This is the amount of kilowatts per year for Loyola.");
+        System.out.println(stfxKW.getKiloPerYear());
+        System.out.println("This is the amount of carbon dioxide produced per year for Loyola.");
+        System.out.println(stfxKW.getCO2PerYear());
+        System.out.println("This is the amount of carbon dioxide a student produces at Loyola.");
+        System.out.println(stfxKW.getCO2PerStudent());
+        System.out.println("This is the amount of trees needed to compensate for the amount of carbon dioxide produced.");
+        System.out.println(stfxKW.getNumberTrees());
+
+        System.out.println(stfxKW.toString());
     }
 }
